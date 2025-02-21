212654d474ee909f8fffc92221efcfa27b558481
JosephSC0121 <josephsilvacasas0121@gmail.com>
2025-02-21 18:37:45 -0500

    feat: add components register type and register page

---
+++ types/register.ts
@@ -0,0 +1,12 @@
+export type RegisterFormData = {
+    username: string;
+    first_name: string;
+    second_name: string;
+    last_name: string;
+    second_last_name: string;
+    email: string;
+    password: string;
+    confirm_password: string;
+    phone: string;
+    address: string;
+};
