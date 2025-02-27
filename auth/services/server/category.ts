d6f9a0eb3893dc2616e66bd51a70c6430fd8821b
JosephSC0121 <josephsilvacasas0121@gmail.com>
2025-02-26 21:34:28 -0500

    chore: refactor SSR CSR fetchs, and add endpoints

---
+++ auth/services/server/category.ts
@@ -0,0 +1,26 @@
+"use server";
+
+const backendUrl = process.env.BACKEND_URL || "";
+
+export const getCategory = async () => {
+    try {
+        const response = await fetch(`${backendUrl}/productos/categorias/`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || "Error al obtener productos.");
+        }
+        return { success: true, data };
+
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "Error desconocido";
+        return { success: false, message: errorMessage };
+    }
+};
+
