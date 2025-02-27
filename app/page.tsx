b130ebc3124f8fdf5b60d5ccb540d4c9eaef915d
JosephSC0121 <josephsilvacasas0121@gmail.com>
2025-02-26 21:28:58 -0500

    feat: add categories bento to main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,25 @@
 import MainLayout from "@/components/layout/main";
 import { title, subtitle } from "@/components/primitives";
 import ProductsList from "@/components/product-list";
-import { getProductsImages } from "@/auth/services/products-image";
+import { getProductsImages } from "@/auth/services/server/products-image";
+import CategoryList from "@/components/category-list";
+import { getCategory } from "@/auth/services/server/category";
 
 export default async function Home() {
   const { success, data, message } = await getProductsImages();
 
-  
   if (!success) {
     return <p>Error: {message}</p>;
   }
 
+  const { success: succes_categories, data: categories, message: message_categories } = await getCategory();
+
+  if (!succes_categories) {
+    return <p>Error: {message_categories}</p>;
+  }
   return (
     <MainLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <section className="flex flex-col items-center justify-center gap-5 py-8 md:py-10">
         <div className="inline-block max-w-xl text-center justify-center">
           <span className={title()}>Compra y vende&nbsp;</span>
           <span className={title({ color: "green" })}>productos&nbsp;</span>
@@ -26,10 +32,20 @@ export default async function Home() {
           </div>
         </div>
       </section>
+      <section className="container mx-auto px-4 py-6">
+        <h2 className={subtitle({ class: "mb-4 text-xl font-semibold" })}>
+          Productos Destacados
+        </h2>
+        <ProductsList products={data} />
+      </section>
 
-      <span className={subtitle()}>Productos Destacados&nbsp;</span>
-
-      <ProductsList products={data} />
+      <section className="container mx-auto px-4 py-6">
+        <h2 className={subtitle({ class: "mb-4 text-xl font-semibold" })}>
+          Categorías
+        </h2>
+        <CategoryList categories={categories}/>
+      </section>
     </MainLayout>
   );
 }
+
---
+++ components/category-list.tsx
@@ -0,0 +1,136 @@
+"use client"
+import { Card, CardHeader, CardFooter, Image, Button, Skeleton } from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+export default function CategoryList({categories} : {categories : any[]}) {
+
+    if (!categories || categories.length === 0) {
+        return <p>No hay productos disponibles.</p>;
+      }    
+      console.log(categories)
+
+    const router = useRouter();
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+    const [loadedImagesCount, setLoadedImagesCount] = useState(0);
+    const totalImages = 6; 
+
+    const handleImageLoad = () => {
+        setLoadedImagesCount(prev => {
+            const newCount = prev + 1;
+            if (newCount >= totalImages) {
+                setImagesLoaded(true);
+            }
+            return newCount;
+        });
+    };
+
+    useEffect(() => {
+        setImagesLoaded(false);
+        setLoadedImagesCount(0);
+    }, []);
+
+    return (
+        <main className="flex items-center justify-center">
+            <Skeleton className="rounded-lg" isLoaded={imagesLoaded}>
+                <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
+                    <Card className="col-span-12 sm:col-span-4 h-[300px]" isPressable onPress={() => router.push(`/productos/${categories[0].id_categoria}`)}>
+                        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                            <p className="text-tiny text-white/60 uppercase font-bold">Buscas</p>
+                            <h4 className="text-white font-medium text-large">{categories[0].nombre}</h4>
+                        </CardHeader>
+                        <Image
+                            removeWrapper
+                            isZoomed
+                            alt="Card background"
+                            className="z-0 w-full h-full object-cover"
+                            src="https://picsum.photos/600/500"
+                            onLoad={handleImageLoad}
+                        />
+                    </Card>
+                    <Card className="col-span-12 sm:col-span-4 h-[300px]" isPressable onPress={() => router.push(`/productos/${categories[1].id_categoria}`)}>
+                        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                            <p className="text-tiny text-white/60 uppercase font-bold">O tal vez</p>
+                            <h4 className="text-white font-medium text-large">{categories[1].nombre}</h4>
+                        </CardHeader>
+                        <Image
+                            removeWrapper
+                            isZoomed
+                            alt="Card background"
+                            className="z-0 w-full h-full object-cover"
+                            src="https://picsum.photos/600/500"
+                            onLoad={handleImageLoad}
+                        />
+                    </Card>
+                    <Card className="col-span-12 sm:col-span-4 h-[300px]" isPressable onPress={() => router.push(`/productos/${categories[2].id_categoria}`)}>
+                        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                            <p className="text-tiny text-white/60 uppercase font-bold">Te puede interesar</p>
+                            <h4 className="text-white font-medium text-large">{categories[2].nombre}</h4>
+                        </CardHeader>
+                        <Image
+                            removeWrapper
+                            isZoomed
+                            alt="Card background"
+                            className="z-0 w-full h-full object-cover"
+                            src="https://picsum.photos/600/500"
+                            onLoad={handleImageLoad}
+                        />
+                    </Card>
+                    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
+                        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                            <p className="text-tiny text-white/60 uppercase font-bold">Nuestros clientes suelen comprar</p>
+                            <h4 className="text-white/90 font-medium text-xl">{categories[3].nombre}</h4>
+                        </CardHeader>
+                        <Image
+                            removeWrapper
+                            alt="Relaxing app background"
+                            className="z-0 w-full h-full object-cover"
+                            src="https://picsum.photos/600/500"
+                            onLoad={handleImageLoad}
+                        />
+                        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                            <div className="flex flex-grow gap-2 items-center">
+                                <Image
+                                    removeWrapper
+                                    alt="Breathing app icon"
+                                    className="rounded-full w-10 h-11 bg-black"
+                                    src="https://picsum.photos/600/500"
+                                    onLoad={handleImageLoad}
+                                />
+                                <div className="flex flex-col">
+                                    <p className="text-tiny text-white/60">{categories[3].nombre} están disponibles</p>
+                                    <p className="text-tiny text-white/60">Échale un vistazo</p>
+                                </div>
+                            </div>
+                            <Button radius="full" size="sm">
+                                Ver
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
+                        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                            <p className="text-tiny text-white/60 uppercase font-bold">¿Ya has probado?</p>
+                            <h4 className="text-black font-medium text-2xl">{categories[4].nombre}</h4>
+                        </CardHeader>
+                        <Image
+                            removeWrapper
+                            alt="Card example background"
+                            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                            src="https://picsum.photos/600/500"
+                            onLoad={handleImageLoad}
+                        />
+                        <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                            <div>
+                                <p className="text-black text-tiny">{categories[4].nombre}</p>
+                                <p className="text-black text-tiny">Échale un vistazo</p>
+                            </div>
+                            <Button className="text-tiny" color="primary" radius="full" size="sm">
+                                Ver
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                </div>
+            </Skeleton>
+        </main>
+    );
+}
