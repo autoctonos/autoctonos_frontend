0dd638801e73ae07a8f515e4ede9b5d6721615df
JosephSC0121 <josephsilvacasas0121@gmail.com>
2025-03-19 20:36:46 -0500

    chore: rename

--- app/dashboard/solicitudes/page.tsx
+++ app/dashboard/solicitudes/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSession } from "next-auth/react";
 import DashboardLayout from "@/components/layout/dashboard";
-import ProductosTable from "@/components/dashboard/table";
+import ProductosTable from "@/components/dashboard/products-user";
 import { useFetchDataById } from "@/auth/services/client/products";
 
 export default function SolicitudesPage() {
