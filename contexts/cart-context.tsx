17204fa6a8fa9bdcc872769d4017495cb155cbc1
EstebanBarrero <barrerodavid10@gmail.com>
2025-06-27 10:48:38 -0500

    chore: changes name and location of the folder

--- contexts/cart-context.tsx
+++ contexts/cart-context.tsx
@@ -1,22 +1,4 @@
-"use client";
-
 import { createContext, useContext, useState, ReactNode } from "react";
-
-type CartItem = {
-  productId: string;
-  quantity: number;
-  price: number;
-  productName: string;
-  image?: string;
-};
-
-type CartContextType = {
-  cartItems: CartItem[];
-  addToCart: (item: CartItem) => void;
-  incrementItem: (productId: string) => void;
-  decrementItem: (productId: string) => void;
-  removeItem: (productId: string) => void;
-};
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
